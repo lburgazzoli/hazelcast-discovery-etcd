f864295c26282843236853ed64a0a4e343b11f3c
lburgazzoli <lburgazzoli@gmail.com>
2015-10-28 15:42:53 +0100

    fix

--- src/main/bash/etcd_travis_run.sh
+++ src/main/bash/etcd_travis_run.sh
@@ -4,8 +4,8 @@
 
 sleep 5
 
-./etcd-dist/etcdctl set /_hazelcast/node1 '{ "host": "127.0.0.1", "port": 9001 , "tags": { "name": "node1" } }'
-./etcd-dist/etcdctl set /_hazelcast/node2 '{ "host": "127.0.0.1", "port": 9002 , "tags": { "name": "node2" } }'
-./etcd-dist/etcdctl set /_hazelcast/node3 '{ "host": "127.0.0.1", "port": 5701 , "tags": { "name": "node3" } }'
-./etcd-dist/etcdctl set /_hazelcast/node4 '{ "host": "127.0.0.1" , "tags": { "name": "node4" } }'
+./etcd-dist/etcdctl set /hazelcast/node1 '{ "host": "127.0.0.1", "port": 9001 , "tags": { "name": "node1" } }'
+./etcd-dist/etcdctl set /hazelcast/node2 '{ "host": "127.0.0.1", "port": 9002 , "tags": { "name": "node2" } }'
+./etcd-dist/etcdctl set /hazelcast/node3 '{ "host": "127.0.0.1", "port": 5701 , "tags": { "name": "node3" } }'
+./etcd-dist/etcdctl set /hazelcast/node4 '{ "host": "127.0.0.1" , "tags": { "name": "node4" } }'
 
